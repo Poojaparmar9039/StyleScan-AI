5dea9cb78bab8206720e504098358dbdf2bd53d6
Poojaparmar9039 <157222585+Poojaparmar9039@users.noreply.github.com>
2025-07-11 17:03:13 +0530

    Update settings.py

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -74,7 +74,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myapp.wsgi.application'
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://stylescanai.onrender.com"
+]
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
